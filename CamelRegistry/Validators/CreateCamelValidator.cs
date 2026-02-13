20c0af87eef6718d79e16873bb647c3375fb6575
Csilla-Gulyas <linagulyas@gmail.com>
2026-02-13 11:36:55 +0100

    Refactor validators: make Color optional for CreateCamelDto

--- CamelRegistry/Validators/CreateCamelValidator.cs
+++ CamelRegistry/Validators/CreateCamelValidator.cs
@@ -8,9 +8,7 @@ namespace CamelRegistry.Validators
         public CreateCamelValidator()
         {
             RuleFor(x => x.Name).NameRule();
-            RuleFor(x => x.Color)
-                .NotEmpty().WithMessage("A szín megadása kötelező.")
-                .ColorRule();
+            RuleFor(x => x.Color).ColorRule();
             RuleFor(x => x.HumpCount).HumpCountRule();
             RuleFor(x => x.LastFed).LastFedRule();
         }
